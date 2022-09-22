a1c6c7727dbcb549e959756ab0b394b57a1a961b
Kolbe Face <kfface@gmail.com>
2022-09-22 15:59:18 -0500

    removed copied code from rugs.py
    removed needless c&p code and instead imported the code from rugmaker.py (the file formerly known as rugs.py

--- rug_from_csv.py
+++ rug_from_csv.py
@@ -1,24 +1,14 @@
 import sys
 import csv
-
-def makeRug(Pattern, Length):
-    rug_pattern = Pattern
-    rug_length = int(Length)
-    pattern_length = len(rug_pattern)
-    long_enough_pattern = "".join([rug_pattern] * (rug_length + 1))
-    i = 0
-    while i < rug_length:
-        print(long_enough_pattern[i : i + pattern_length])
-        i += 1
-
+import rugmaker
 if len(sys.argv) != 2:
     print("usage: python rug_from_csv.py file.csv")
     exit
 rugs = []
-with open(sys.argv[1], 'r', newline='\n') as file:
+with open('rug_requests.csv', 'r', newline='\n') as file:
     rugDict = csv.DictReader(file)
     for rug in rugDict:
         rug["Length"] = int(rug["Length"])
         rugs.append(rug)
 for rug in rugs:
-    makeRug(rug['Pattern'], rug['Length'])
+    rugmaker.makeRug(rug['Pattern'], rug['Length'])
