9c227edb097f8301e04434dbff3e131f898a92e0
Kolbe Face <kfface@gmail.com>
2022-10-02 11:12:10 -0500

    forced conflict
    I switched the solutions to force a conflict with aki's latest commit removing this solution

--- rugmaker.py
+++ rugmaker.py
@@ -9,26 +9,26 @@ def main():
     makeRug(Pattern, Length)
 
 def makeRug(Pattern, Length):
-    pattern_length = len(Pattern)
-    for i in range(Length):
-        if i % pattern_length == 0:
-            print(Pattern)
-        else:
-            x = (Pattern[i % pattern_length : pattern_length])
-            x += Pattern[0 : (i % pattern_length)]
-            print(x)
+#    pattern_length = len(Pattern)
+#    for i in range(Length):
+#        if i % pattern_length == 0:
+#            print(Pattern)
+#        else:
+#            x = (Pattern[i % pattern_length : pattern_length])
+#            x += Pattern[0 : (i % pattern_length)]
+#            print(x)
 
 
 #alt solution
-#
-#    Length = Pattern
-#    rug_length = int(Length)
-#    pattern_length = len(Length)
-#    long_enough_pattern = "".join([Length] * (rug_length + 1))
-#    i = 0
-#    while i < rug_length:
-#        print(long_enough_pattern[i : i + pattern_length])
-#        i += 1
-#
+
+    Length = Pattern
+    rug_length = int(Length)
+    pattern_length = len(Length)
+    long_enough_pattern = "".join([Length] * (rug_length + 1))
+    i = 0
+    while i < rug_length:
+        print(long_enough_pattern[i : i + pattern_length])
+        i += 1
+
 if __name__ == '__main__':
     main()
