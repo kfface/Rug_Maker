b55de2985a26634b5ab2b6e6e8b30e8aea3e5612
Kolbe Face <kfface@gmail.com>
2022-10-02 13:30:39 -0500

    added combine rugs function
    this is the start of a merge conflict

--- rug_from_csv.py
+++ rug_from_csv.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import rugmaker
+
 if len(sys.argv) != 2:
     print("usage: python rug_from_csv.py file.csv")
     exit
@@ -11,9 +12,14 @@ with open('rug_requests.csv', 'r', newline='\n') as file:
         rug["Length"] = int(rug["Length"])
         rug["Quantity"] = int(rug["Quantity"])
         rugs.append(rug)
-        
+
 for rug in rugs:
     count = 0
     while count < rug['Quantity']:
         rugmaker.makeRug(rug['Pattern'], rug['Length'])
         count += 1
+print("Now let's combine these rugs")
+def rugCombine(rug1, rug2):
+    rugmaker.makeRug(rug1['Pattern'], rug2['Length'])
+    rugmaker.makeRug(rug2['Pattern'], rug1['Length'])
+rugCombine(rugs[1],rugs[2])
