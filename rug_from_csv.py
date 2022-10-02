a135e14685d25ed91364dd438aba0b0b7b4d2b0e
Kolbe Face <kfface@gmail.com>
2022-10-02 12:38:36 -0500

    added step one to merge conflict

--- rug_from_csv.py
+++ rug_from_csv.py
@@ -5,13 +5,13 @@ if len(sys.argv) != 2:
     print("usage: python rug_from_csv.py file.csv")
     exit
 rugs = []
-with open('rug_requests.csv', 'r', newline='\n') as file:
+with open(sys.argv[1], 'r', newline='\n') as file:
     rugDict = csv.DictReader(file)
     for rug in rugDict:
         rug["Length"] = int(rug["Length"])
         rug["Quantity"] = int(rug["Quantity"])
         rugs.append(rug)
-        
+
 for rug in rugs:
     count = 0
     while count < rug['Quantity']:
